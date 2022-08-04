a31b91cb15445a2641dd6c2ed2e0a426b0086f56
vhillyjay <107127737+vhillyjay@users.noreply.github.com>
2022-08-04 10:40:42 +0800

    database/migrations table

---
+++ database/migrations/2022_08_04_023740_create_supplies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            // creates created_at and updated_at
+            $table->string('name');
+            $table->string('type');
+            $table->string('brand');
+            $table->integer('price');
+            $table->integer('quantity');
+            // $table->double('price', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supplies');
+    }
+};
