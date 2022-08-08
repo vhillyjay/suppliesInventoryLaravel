a47c267c1adc6f358cd68c6d786059d8cf700772
vhillyjay <107127737+vhillyjay@users.noreply.github.com>
2022-08-08 14:00:32 +0800

    logout redirect

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -215,10 +215,21 @@
                                     Activity Log
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
+
+                                <!-- Authentication Logout -->
+                                <a class="dropdown-item" href="#">
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+
+                                        <x-dropdown-link :href="route('logout')"
+                                                onclick="event.preventDefault();
+                                                            this.closest('form').submit();">
+                                            {{ __('Log Outsss') }}
+                                        </x-dropdown-link>
+                                    </form>
                                 </a>
+                                <!-- Authentication Logout -->
+
                             </div>
                         </li>
 
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/tryindex', function () {
-//     return view('supplies.show');
-// });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
