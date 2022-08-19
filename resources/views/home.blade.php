ec369bda58444714a974f9cd20bded0045b70465
vhillyjay <107127737+vhillyjay@users.noreply.github.com>
2022-08-19 22:46:54 +0800

    /home number of supplies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,11 +12,11 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Total Number of Supplies</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">  </div>
+                                Total Number of Supplies in Stock</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"> {{ $totalCounter }} </div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            <i class="bi bi-calendar fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
@@ -30,8 +30,8 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Earnings (Annual)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                Gross Amount of Stocks</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">₱ </div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
