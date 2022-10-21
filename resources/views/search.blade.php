ff64e1ab6918f398c2026a8d8b7f0992ce3f2ca2
vhillyjay <107127737+vhillyjay@users.noreply.github.com>
2022-10-21 16:22:13 +0800

    search box 2
    now working. just have to put the output on modals.

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -6,6 +6,20 @@ Supplies <br>
     {{ $searchSuppliesData->name }} <br>
 @endforeach
 
+Transac <br>
+@foreach($searchTransaction as $searchTransactionData)
+    {{ $searchTransactionData->product_name }} <br>
+@endforeach
 
+Trans issued by <br>
+@foreach($searchTransaction as $searchTransactionData)
+    {{ $searchTransactionData->issued_by }} <br>
+@endforeach
+
+<br>
+paging paging<br>
+@foreach($sampling as $samplingData)
+    {{ $samplingData->id }} <br>
+@endforeach
 
 @endsection
